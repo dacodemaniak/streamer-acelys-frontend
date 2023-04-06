9a5cd34db59c8f6a21905c22236c0f035ba348ea
DaCoDeMaNiaK <jla.webprojet@gmail.com>
2023-04-06 14:45:52 +0200

    Remove MenuComponent Replace by Material Menu

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,18 +1,15 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
 import { SharedModule } from '../shared/shared.module';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { AppInitializerService, appInitializer } from './services/app-initializer.service';
-import { UserMenuComponent } from './components/user-menu/user-menu.component';
-
-
 
 @NgModule({
   declarations: [
     HeaderComponent,
     FooterComponent,
-    UserMenuComponent
+
+
   ],
   imports: [
     SharedModule
