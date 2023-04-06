225db228a1dca68d7013a821cf1016697ec9fe9a
DaCoDeMaNiaK <jla.webprojet@gmail.com>
2023-04-06 14:47:14 +0200

    Add login property

--- src/app/user/types/user.type.ts
+++ src/app/user/types/user.type.ts
@@ -2,10 +2,11 @@ import { UserRoleType } from "./user-role.type"
 
 export type UserType = {
   id?: number
+  login: string
   lastName: string
   firstName: string
   phoneNumber?: string
   email: string
-  roles: UserRoleType
+  role: UserRoleType
   jwtToken: string
 }
