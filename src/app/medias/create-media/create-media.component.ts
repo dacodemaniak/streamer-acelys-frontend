e4ccac92c9e804371522a01fba23d6d117f27430
Kevin Lam Seck <kevin.lamseck@gmail.com>
2023-04-12 00:25:05 +0200

    reformat

--- src/app/medias/create-media/create-media.component.ts
+++ src/app/medias/create-media/create-media.component.ts
@@ -1,64 +1,41 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
-import { AbstractControl, FormBuilder, FormGroup, Validators } from '@angular/forms';
+import {
+  AbstractControl,
+  FormBuilder,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-create-media',
   templateUrl: './create-media.component.html',
-  styleUrls: ['./create-media.component.scss']
+  styleUrls: ['./create-media.component.scss'],
 })
 export class CreateMediaComponent implements OnInit {
-
-
   @Input() visibility: boolean = false;
   @Output() newItemEvent = new EventEmitter<boolean>();
 
-  public mediaForm: FormGroup = new FormGroup({})
+  public mediaForm: FormGroup = new FormGroup({});
 
   constructor(private _formBuilder: FormBuilder) { }
 
   ngOnInit(): void {
-
     this.mediaForm = this._formBuilder.group({
-      title: [
-        '',
-        [
-          Validators.required,
-          Validators.minLength(8)
-        ]
-      ],
-      summary: [
-        ''
-      ],
-      duration: [
-        '',
-        [
-          Validators.required
-        ]
-      ],
-      url: [
-        '',
-        [
-          Validators.required
-        ]
-      ],
-      typeMedia: [
-        '',
-        [
-          Validators.required
-        ]
-      ]
-    })
+      title: ['', [Validators.required, Validators.minLength(8)]],
+      summary: [''],
+      duration: ['', [Validators.required]],
+      url: ['', [Validators.required]],
+      typeMedia: ['', [Validators.required]],
+    });
   }
 
   get c(): { [key: string]: AbstractControl } {
-    return this.mediaForm.controls
+    return this.mediaForm.controls;
   }
 
-  onSubmit(): void {
-  }
+  onSubmit(): void { }
 
   onNoClick() {
     this.newItemEvent.emit(false);
   }
-
 }
