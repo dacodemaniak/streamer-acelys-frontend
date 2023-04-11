0bcee4addf47e7386e8ba76622dc88411cf16094
Kevin Lam Seck <kevin.lamseck@gmail.com>
2023-04-11 17:18:10 +0200

    Add routon to create a new media

--- src/app/dashboard/dashboard.module.ts
+++ src/app/dashboard/dashboard.module.ts
@@ -1,13 +1,14 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { MediasModule } from '../medias/medias.module';
 import { SharedModule } from '../shared/shared.module';
+import { BreadcrumbComponent } from './components/breadcrumb/breadcrumb.component';
 import { TileComponent } from './components/tile/tile.component';
 import { DashboardRoutingModule } from './dashboard-routing.module';
 import { DashboardComponent } from './dashboard.component';
 import { ConceptorComponent } from './pages/conceptor/conceptor.component';
 import { ManagerComponent } from './pages/manager/manager.component';
 import { StudentComponent } from './pages/student/student.component';
-import { BreadcrumbComponent } from './components/breadcrumb/breadcrumb.component';
 
 
 
@@ -23,6 +24,7 @@ import { BreadcrumbComponent } from './components/breadcrumb/breadcrumb.componen
   imports: [
     CommonModule,
     SharedModule,
+    MediasModule,
     DashboardRoutingModule
   ]
 })
