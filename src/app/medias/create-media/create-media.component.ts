105ace9cde8b9d91b9cffa22d1340d2011fa1e56
Kevin Lam Seck <kevin.lamseck@gmail.com>
2023-04-12 11:46:05 +0200

    can create a media with just a url

--- src/app/medias/create-media/create-media.component.ts
+++ src/app/medias/create-media/create-media.component.ts
@@ -5,6 +5,14 @@ import {
   FormGroup,
   Validators,
 } from '@angular/forms';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { Router } from '@angular/router';
+import { take } from 'rxjs';
+import { LocalStorageService } from 'src/app/core/services/local-storage.service';
+import { MediaType } from 'src/app/course/types/media-type';
+import { ModuleService } from 'src/app/modules/services/module.service';
+import { Member } from 'src/app/user/models/member';
+import { MediaService } from '../services/media.service';
 
 @Component({
   selector: 'app-create-media',
@@ -19,22 +27,24 @@ export class CreateMediaComponent implements OnInit {
   public selectedOption: string = '';
 
   public options = new Map<string, number>([
-    ["Video", 1],
-    ["Slide", 2],
-    ["Document", 3],
-    ["Audio", 4],
-    ["Image", 5],
-    ["Animation", 6],
-    ["Interactive", 7],
-    ["PDF", 8]
-  ])
+    ['Video', 1],
+    ['Slide', 2],
+    ['Document', 3],
+    ['Audio', 4],
+    ['Image', 5],
+    ['Animation', 6],
+    ['Interactive', 7],
+    ['PDF', 8],
+  ]);
 
-
-  get optionsMethod() {
-    return Array.from(this.options.keys())
-  }
-
-  constructor(private _formBuilder: FormBuilder) { }
+  constructor(
+    private _formBuilder: FormBuilder,
+    private _mediaService: MediaService,
+    private _moduleService: ModuleService,
+    private _router: Router,
+    private _localStorageService: LocalStorageService,
+    private _snackBar: MatSnackBar
+  ) { }
 
   ngOnInit(): void {
     this.mediaForm = this._formBuilder.group({
@@ -50,8 +60,34 @@ export class CreateMediaComponent implements OnInit {
     return this.mediaForm.controls;
   }
 
+  get optionsMethod() {
+    return Array.from(this.options.keys());
+  }
+
   onSubmit(): void {
-    console.log(this.mediaForm.value);
+    // Search the number from map options based on the name
+    const typeMediaID = this.options.get(this.mediaForm.value.typeMedia);
+    const conceptor: Member = this._localStorageService.getMemberFromStorage()
+    const newConceptor: any = { id: conceptor.id }
+    const typeMedia: any = { id: typeMediaID, value: this.mediaForm.value.typeMedia }
+    const media: MediaType = {
+      title: this.c['title'].value,
+      summary: this.c['summary'].value,
+      duration: this.c['duration'].value,
+      url: this.c['url'].value,
+      typeMedia: typeMedia,
+      creator: newConceptor
+    }
+    console.log(media);
+    this._mediaService.add(media).pipe(take(1)).subscribe(
+      {
+        next: (response: any) => {
+          console.log(response);
+        }
+      }
+    )
+
+
   }
 
   onNoClick() {
