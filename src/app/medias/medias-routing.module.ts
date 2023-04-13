f4f4e0671f82eb71d923d34974feb7b3a88b2f51
Kevin Lam Seck <kevin.lamseck@gmail.com>
2023-04-13 15:40:36 +0200

    Add the Component for display the list of media

--- src/app/medias/medias-routing.module.ts
+++ src/app/medias/medias-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CreateMediaComponent } from './create-media/create-media.component';
+import { ListMediaComponent } from './list-media/list-media.component';
 import { UpdateMediaComponent } from './update-media/update-media.component';
 
 
@@ -24,6 +25,11 @@ export class MediasRoutingModule {
       path: 'update',
       component: UpdateMediaComponent,
       data: { title: 'Dashboard | Update Media', breadcrumb: 'Update Media' },
+    },
+    {
+      path: 'list',
+      component: ListMediaComponent,
+      data: { title: 'Dashboard | Managed My Media', breadcrumb: 'Managed Medias' },
     },
     // {
     //   path: '**',
