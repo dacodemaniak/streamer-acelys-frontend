ada795ca5413541f9ccbdb253d3cf4fbe17505d3
Kevin Lam Seck <kevin.lamseck@gmail.com>
2023-04-19 11:42:41 +0200

    change the organization of the medias folder

--- src/app/medias/medias.module.ts
+++ src/app/medias/medias.module.ts
@@ -2,11 +2,11 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 
 import { SharedModule } from '../shared/shared.module';
-import { CreateMediaComponent } from './create-media/create-media.component';
-import { MediasRoutingModule } from './medias-routing.module';
-import { UpdateMediaComponent } from './update-media/update-media.component';
-import { ListMediaComponent } from './list-media/list-media.component';
 import { MediaTileComponent } from './components/media-tile/media-tile.component';
+import { MediasRoutingModule } from './medias-routing.module';
+import { CreateMediaComponent } from './pages/create-media/create-media.component';
+import { ListMediaComponent } from './pages/list-media/list-media.component';
+import { UpdateMediaComponent } from './pages/update-media/update-media.component';
 
 
 
